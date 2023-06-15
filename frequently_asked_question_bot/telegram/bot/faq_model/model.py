d826d539b0ba23d599674a579ca8ce925b0cdcf2
Yuriy Peshkichev <yuri.peshkichev@gmail.com>
2023-06-15 16:06:06 +0700

    dox encoding

--- frequently_asked_question_bot/telegram/bot/faq_model/model.py
+++ frequently_asked_question_bot/telegram/bot/faq_model/model.py
@@ -207,7 +207,7 @@ def find_similar_questions(question: str):
     seen = []
     if model[cur_index] == "Luyu/co-condenser-marco-retriever":
         top_list = []
-        for el in sorted(filter(lambda x: x[1] > 0.1, emb_with_scores), key=lambda x: x[1])[-30:]:
+        for el in sorted(filter(lambda x: x[2] > 0.1, emb_with_scores), key=lambda x: x[2])[-30:]:
             top_list.append((el[0],el[1],test_ce(question,dox[el[1]])))
 
         sorted_tuple = sorted(top_list, key=lambda el: el[2],reverse=True)
